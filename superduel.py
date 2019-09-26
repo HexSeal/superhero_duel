fb61cd35345ffd7c1296c7ed9a21adc29a151237
HexSeal <maxfin13@gmail.com>
2019-09-26 14:42:48 -0700

    [fixed] Everything. It finally works + some extra functionality

--- superduel.py
+++ superduel.py
@@ -23,84 +23,6 @@ class Weapon(Ability):
         half_dmg = self.max_damage // 2
         return random.randint(half_dmg, int(self.max_damage))
 
-class Team:
-    def __init__(self, name):
-        self.name = []
-        self.heroes = []
-
-    def add_hero(self, hero): 
-        self.heroes.append(hero)
-
-    def remove_hero(self, name):
-        for hero in self.heroes:
-            if hero.name == name:
-                self.heroes.remove(hero)
-                break
-            else:
-                return 0
-
-    def view_all_heroes(self):
-        for hero in self.heroes:
-            print(hero.name)
-
-    def attack(self, other_team):
-        ''' Got from this comment to the next from github.com/soundreaper/superheroteamdueler. This is so that the code can recognize that the fight is over because heroes can't be
-        removed from heroes but we need to know which are done '''
-        ''' github.com/soundreaper/superheroteamdueler --start-- '''
-        alive_hero_list_1 = []
-        alive_hero_list_2 = []
-
-        for hero_1 in self.heroes:
-                if Hero.is_alive == True:
-                    alive_hero_list_1.append(self.heroes.index(hero_1))
-        
-        for hero_2 in other_team.heroes:
-                if Hero.is_alive == True:
-                    alive_hero_list_2.append(other_team.heroes.index(hero_2))
-
-        ''' battle each team against each other. '''
-        while len(alive_hero_list_1) > 0 and len(alive_hero_list_2) > 0:
-            Hero.fight(random.choice(alive_hero_list_1), random.choice(alive_hero_list_2))
-
-            for hero_alivecheck_1 in self.heroes:
-                if hero_alivecheck_1.status == "Dead":
-                    alive_hero_list_1.pop(self.heroes.index(hero_alivecheck_1))
-            
-            for hero_alivecheck_2 in other_team.heroes:
-                if hero_alivecheck_2.status == "Dead":
-                    alive_hero_list_2.pop(other_team.heroes.index(hero_alivecheck_2))
-
-        if len(alive_hero_list_1) > 0:
-            return self.name
-        elif len(alive_hero_list_2) > 0:
-            return other_team.name
-        elif len(alive_hero_list_1) == len(alive_hero_list_2):
-            return "Draw!"
-        ''' --end-- '''
-
-    def revive_heroes(self, starting_health=100):
-        ''' Resets a hero back to starting health'''
-        for hero in self.heroes:
-            hero.current_health = starting_health
-
-    def stats(self):
-        for hero in self.heroes:
-            print('Survivors: ')
-            if hero.is_alive():
-                print(hero.name, self.name)
-
-                if hero.deaths == 0: 
-                    hero.deaths = 1
-                    print('{} K/D ratio: {}'.format((hero.name), (hero.kills)/(hero.deaths)), ' with {} kills and {} deaths'.format(hero.kills, hero.deaths))
-                else:
-                    print('{} K/D ratio: {}'.format((hero.name), (hero.kills)/(hero.deaths)), ' with {} kills and {} deaths'.format(hero.kills, hero.deaths))
-
-            elif hero.is_alive() == False:
-                defeated = []
-                
-        print('Defeated Heroes:')
-        for hero in defeated:
-            print('{} K/D ratio: {}'.format(hero.name, (hero.kills)/(hero.deaths)))
 class Hero:
     def __init__(self, name, starting_health=100):
         self.abilities = []
@@ -142,41 +64,106 @@ class Hero:
 
     def add_kills(self, kill_count):
         self.kills += kill_count
+        return self.kills
 
     def add_deaths(self, num_deaths):
         self.deaths += num_deaths
+        return self.deaths
 
     def add_weapon(self, weapon):
         self.abilities.append(weapon)
- 
 
     def fight(self, opponent):
-        fighting = True
-        while fighting:
+        while self.is_alive() and opponent.is_alive():
             self_damage = self.attack()
             enemy_damage = opponent.attack()
 
             self.take_damage(enemy_damage)
             opponent.take_damage(self_damage)
 
-            if self.is_alive() == False:
-                print(
-                    f'{opponent.name} knocked out {self.name}!')   
-                self.add_deaths(1)
-                opponent.add_kills(1)
-                fighting = False
+        if self.is_alive() == False:
+            print(f'{opponent.name} knocked out {self.name}!')   
+            self.add_deaths(1)
+            opponent.add_kills(1)
 
-            elif opponent.is_alive() == False:
-                print(f'{self.name} knocked out {opponent.name}!')
-                self.add_kills(1)
-                opponent.add_deaths(1)
-                fighting = False
-            
+        else: 
+            print(f'{self.name} knocked out {opponent.name}!')
+            self.add_kills(1)
+            opponent.add_deaths(1)
+
+class Team:
+    def __init__(self, name):
+        self.name = name
+        self.heroes = []
+
+    def add_hero(self, hero): 
+        self.heroes.append(hero)
+
+    def remove_hero(self, name):
+        for hero in self.heroes:
+            if hero.name == name:
+                self.heroes.remove(hero)
+                break
             else:
-                continue
+                return 0
 
+    def view_all_heroes(self):
+        for hero in self.heroes:
+            print(hero.name)
+
+    def attack(self, other_team):
+        ''' Got from this comment to the next from github.com/soundreaper/superheroteamdueler. This is so that the code can recognize that the fight is over because heroes can't be
+        removed from heroes but we need to know which are done '''
+        ''' github.com/soundreaper/superheroteamdueler --start-- '''
+        alive_hero_list_1 = []
+        alive_hero_list_2 = []
+
+        for hero_1 in self.heroes:
+                if hero_1.is_alive() == True:
+                    alive_hero_list_1.append(self.heroes.index(hero_1))
+        
+        for hero_2 in other_team.heroes:
+                if hero_2.is_alive() == True:
+                    alive_hero_list_2.append(other_team.heroes.index(hero_2))
+
+        ''' battle each team against each other. '''
+        while len(alive_hero_list_1) > 0 and len(alive_hero_list_2) > 0:
+            random_hero_1 = self.heroes[random.choice(alive_hero_list_1)]
+            random_hero_2 = other_team.heroes[random.choice(alive_hero_list_2)]
+
+            random_hero_1.fight(random_hero_2)
+
+            for hero_alivecheck_1 in self.heroes:
+                if hero_alivecheck_1.is_alive() == False:
+                    alive_hero_list_1.pop(self.heroes.index(hero_alivecheck_1))
+            
+            for hero_alivecheck_2 in other_team.heroes:
+                if hero_alivecheck_2.is_alive() == False:
+                    alive_hero_list_2.pop(other_team.heroes.index(hero_alivecheck_2))
+
+        if len(alive_hero_list_1) > 0:
+            return self.name
+        elif len(alive_hero_list_2) > 0:
+            return other_team.name
+        elif len(alive_hero_list_1) == len(alive_hero_list_2):
+            return "Draw!"
+        ''' --end-- '''
+
+    def revive_heroes(self, starting_health=100):
+        ''' Resets a hero back to starting health'''
+        for hero in self.heroes:
+            hero.current_health = starting_health
+
+    def stats(self):
+        for hero in self.heroes:
+            if hero.deaths == 0:
+                print('{}:\nK/D ratio: {}'.format((hero.name), (hero.kills)), 'with {} kills and 0 deaths'.format(hero.kills))
+            else:
+                print('{}:\nK/D ratio: {}'.format((hero.name), (hero.kills) // (hero.deaths)), 'with {} kills and {} deaths'.format(hero.kills, hero.deaths))
+                   
 class Arena:
     def __init__(self):
+        self.winning_team = None
         print('\nWelcome to the Arena.\n')
         t1_name = input('What do you want the first team to be called? ')
         self.team_one = Team(t1_name)
@@ -229,7 +216,7 @@ class Arena:
                 armor = self.create_armor()
                 hero.add_armor(armor)
 
-                more_armor = input('Want to add more armor? y/n ')
+                more_armor = input('Want to add more armor? y/n \n')
                 if 'y' not in more_armor:
                     break
         return hero
@@ -252,25 +239,16 @@ class Arena:
         else:
             t2_num = input('Incorrect input. Please enter a number: ')
             
-
     def team_battle(self):
         ''' Puts team one against team two '''
         self.winning_team = self.team_one.attack(self.team_two)
-        return self.winning_team
 
     def show_stats(self):
+        print("\nStats: ")
         self.team_one.stats()
         self.team_two.stats()
-
-        print("The winning team is: " + self.winning_team)
-
-        for hero in self.team_one.heroes:
-            if hero.is_alive():
-                print('Team One is Victorious!')
-                break
-            else:
-                print('Team Two is Victorious!')
-                break
+        
+        print("\nThe winning team is: \n" + self.winning_team)
 
 if __name__ == "__main__":
     game_is_running = True
